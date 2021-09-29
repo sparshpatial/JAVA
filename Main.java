981848808de4bbcd0111a15e4733bd2366035648
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-09-29 08:45:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ package com.company;
 public class Main{
     public static void main(String agrs[])
     {
-        System.out.println("hello world !.This is the fist main.java");
+        System.out.println("Hello world !.This is the fist main.java");
     }
 }
