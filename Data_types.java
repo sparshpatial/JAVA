247f795cb3a1bd5405b332f068e9eea4cc40417d
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-09-30 08:58:55 +0530

    Add files via upload

---
+++ Data_types.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Data_types {
+    public static void main(String[]args){
+        int a = 100;
+        int b = 200;
+        double c = 300;
+        double d = 400;
+        var name = "Sparsh";
+        String lastname = " Patial";
+        int e = a + b;
+        double f = c + d;
+        System.out.println("Hello my name is "+ name + lastname);
+        System.out.println("The sum of " + a +" & "+ b + " = " +e);
+        System.out.println("The sum of " + c +" & "+ d + " = " +f);
+    }
+}
