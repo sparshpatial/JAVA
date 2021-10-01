cb0d6d277e9b98dc228d282e8b39f1e55d376bbc
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-01 08:41:33 +0530

    Update Dynamic_Initialization.java

--- Dynamic_Initialization.java
+++ Dynamic_Initialization.java
@@ -3,7 +3,7 @@ package com.company;
 public class Dynamic_Initialization {
     public static void main (String [] args){
       double a=3.0 , b=4.0 ;
-      double c=Math.sqrt(a*a + b*b);
+      double c=Math.sqrt(a*a + b*b);//Here c variable is dynamically initialized
       System.out.println("hypotenuse is "+c);
     }
 }
