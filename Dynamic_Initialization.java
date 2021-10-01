6da10e36e895b26f37fde2dedd2df16ed4679964
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-01 08:40:55 +0530

    Add files via upload

---
+++ Dynamic_Initialization.java
@@ -0,0 +1,9 @@
+package com.company;
+//DYNAMIC INITIALIZATION
+public class Dynamic_Initialization {
+    public static void main (String [] args){
+      double a=3.0 , b=4.0 ;
+      double c=Math.sqrt(a*a + b*b);
+      System.out.println("hypotenuse is "+c);
+    }
+}
