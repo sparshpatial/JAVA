95ee6ea9f928d2f4972dd9527fecb7161882413c
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-02 11:26:49 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Variables {
+    public static void main(String args[]){
+    int a = 9, b = 7;
+    int c = a + b;
+    System.out.println("Sum of a and b is:"+c );
+}
+}
