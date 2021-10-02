3dea7f5581cdec5b5af7b2a4597f24c8bccaef7e
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-02 11:27:49 +0530

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -3,7 +3,7 @@ package com.company;
 public class Variables {
     public static void main(String args[]){
     int a = 9, b = 7;//a,b,c are variables
-    int c = a + b;
+    int c = a + b;//Sum of variables
     System.out.println("Sum of a and b is:"+c );
 }
 }
