374d76f4f55d651647bc3e2898f1fdde8af06dc5
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-03 11:17:36 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Casting {
+    public static void main(String[] args) {
+        int a=257;
+        byte b;
+        double c=323.142;
+
+        System.out.println("This is the conversion of int into byte");
+        b=(byte) a;
+        System.out.println("a and b "+ a +" "+b);
+        System.out.println("This is the conversion of double into int ");
+        a=(int) c;
+        System.out.println("a and b "+ c +" "+a);
+        System.out.println("This is the conversion of double into byte ");
+        b=(byte) c;
+        System.out.println("a and b "+ c +" "+b);
+    }
+}
