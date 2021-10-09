84b98f710b520ca3a92081f7c64bdc5d2376adcc
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-09 11:44:43 +0530

    Add files via upload

---
+++ loops/for_loop.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class for_loop {
+    public static void main(String[] args) {
+        int i= 10;
+        for(i=0;i<=10;i++){
+            System.out.println("You are on"+i+" position.");
+        }
+    }
+}
