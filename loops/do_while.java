91aa89f12d7e99d10e5ca88b27b4928567f5d9c7
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-11 11:00:41 +0530

    Add files via upload

---
+++ loops/do_while.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class do_while {
+    public static void main(String[] args) {
+        int n=10;
+        do{
+
+            System.out.println("You are on "+n+" position.");
+            n--;
+        }while(n>0);
+    }
+}
