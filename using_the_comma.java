81827fe49cc0072a47533df5d61710fd2d2aa23d
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2021-10-19 21:15:47 +0530

    Add files via upload

---
+++ using_the_comma.java
@@ -0,0 +1,24 @@
+package com.company;
+
+//public class Sample {
+   // public static void main(String[] args) {
+        //int a,b;
+      //  b=4;
+       // for(a=1;a<b;a++){
+       //     System.out.println("a="+a);
+      //      System.out.println("b="+b);
+     //       b--;
+     //   }
+   // }
+//}
+public class using_the_comma {
+ public static void main(String[] args) {
+int a,b;
+
+ for(a=1,b=4;a<b;a++,b--){
+     System.out.println("a="+a);
+      System.out.println("b="+b);
+       b--;
+  }
+ }
+}
