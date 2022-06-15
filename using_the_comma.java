398de14d62efaadf951c499e2e03548d3fcdcaeb
Sparsh Patial <69122849+sparshpatial@users.noreply.github.com>
2022-06-15 17:21:04 +0530

    Update using_the_comma.java

--- using_the_comma.java
+++ using_the_comma.java
@@ -1,16 +1,6 @@
 package com.company;
 
-//public class Sample {
-   // public static void main(String[] args) {
-        //int a,b;
-      //  b=4;
-       // for(a=1;a<b;a++){
-       //     System.out.println("a="+a);
-      //      System.out.println("b="+b);
-     //       b--;
-     //   }
-   // }
-//}
+
 public class using_the_comma {
  public static void main(String[] args) {
 int a,b;
